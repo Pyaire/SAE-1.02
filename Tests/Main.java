6f509bb5f2d8cd111c1d58a57ac153fa5cd21427
Pierre Ansel <pierre.ansel03@gmail.com>
2021-12-08 15:32:57 +0100

    Co-authored-by: GurvannB <GurvannB@users.noreply.github.com>

--- Tests/Main.java
+++ Tests/Main.java
@@ -18,34 +18,64 @@ class Main extends Program {
         } while(capital>0);
     }
 
-    // boolean estNombre(String action) {
-    //     boolean estValide = true;
-    //     if (length(action) == 0) {
-    //         estValide = false;
-    //     }
-    //     int idx = 0;
-    //     while (estValide && idx < length(action)) {
-    //         if (chartAt(action, idx) < '0' || charAt(action, idx) > '9') {
-    //             estValide = false;
-    //         }
-    //         idx++;
-    //     }
+    boolean strToIntPossible(String action){
+        boolean fin = true;
+        int retour = 0;     //mais pas de retour car la fonction et un boolean !
+        int i = 0;
+        while (fin && i<length(action)) {
+            if (charAt(action, i) < '0' || charAt(action, i) > '9') {
+                fin = false;
+            }
+            i++;
+        }
+        return fin;
+    }
 
-    //     return estValide;
-    // }
+    int strToInt(String action) {
+        int retour = 0;
+        if(strToIntPossible(action)){
+            for (int i = 0; i<length(action); i++){
+                retour += (int) (charAt(action, i) - '0') * pow(10, (length(action) - i));
+            }
+        }
+        return retour;
+    }
 
     int choisir() {
         boolean estValide = false;
-        int choix;
+        String choix;
+        int choix_final;
         do {
+            clearScreen();
             println("1: Récupérer l'argent");
             println("2: Améliorer la production");
             println("3: Construire");
 
-            choix = readInt();
-            estValide = true;
+            choix = readString(5000);
+            estValide = strToIntPossible(choix);
+            if (!estValide) {
+                println("Il semblerait que vous n'ayez pas entré un nombre ! Veuillez réessayer !");
+            }
         } while (!estValide);
-        return choix;
+        choix_final = strToInt(choix);
+        return choix_final;
+    }
+
+    void testEstValide(){
+        assertTrue(strToIntPossible("674265"));
+        assertFalse(strToIntPossible("a5642"));
+        assertFalse(strToIntPossible("395678687G"));
+        assertFalse(strToIntPossible(" 6"));
+        assertFalse(strToIntPossible(""));
+    }
+
+    void teststrToInt(){
+        assertEquals(674265,strToInt("674265"));
+        assertEquals(5642,strToInt("5642"));
+        assertEquals(395678687,strToInt("395678687"));
+        assertEquals(5395,strToInt("5395"));
+        assertEquals(32245,strToInt("32245"));
+        assertNotEquals(10,strToInt("38"));
     }
     
 }
